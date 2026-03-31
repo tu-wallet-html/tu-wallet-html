029c87291ae3eb6a05f3fbddb7b20370d537cc31
tu-wallet-html <support@cryptoblockchain.my>
2026-03-31 16:44:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+const PASSWORD = "1234"; // clave fija
+
+let user = localStorage.getItem("user") || "";
+
+let wallet = JSON.parse(localStorage.getItem("wallet")) || {
+  BTC: 0,
+  ETH: 0,
+  USDT: 0,
+  BNB: 0
+};
+
+let history = JSON.parse(localStorage.getItem("history")) || [];
+
+checkLogin();
+updateUI();
+
+function login() {
+  const dni = document.getElementById("dni").value;
+  const pass = document.getElementById("password").value;
+
+  if (pass !== PASSWORD) {
+    alert("Contraseña incorrecta");
+    return;
+  }
+
+  if (dni === "") {
+    alert("Introduce DNI");
+    return;
+  }
+
+  user = dni;
+  localStorage.setItem("user", user);
+
+  checkLogin();
+  updateUI();
+}
+
+function logout() {
+  localStorage.removeItem("user");
+  location.reload();
+}
+
+function checkLogin() {
+  if (user) {
+    document.getElementById("loginCard").style.display = "none";
+    document.getElementById("walletApp").style.display = "block";
+    document.getElementById("userText").innerText = "Usuario: " + user;
+  }
+}
+
+function addMoney() {
+  const crypto = document.getElementById("crypto").value;
+  const amount = Number(document.getElementById("amount").value);
+
+  if (amount > 0) {
+    wallet[crypto] += amount;
+    history.unshift("➕ " + user + " añadió " + amount + " " + crypto);
+    save();
+  }
+}
+
+function removeMoney() {
+  const crypto = document.getElementById("crypto").value;
+  const amount = Number(document.getElementById("amount").value);
+
+  if (amount > 0) {
+    wallet[crypto] -= amount;
+    history.unshift("➖ " + user + " quitó " + amount + " " + crypto);
+    save();
+  }
+}
+
+function save() {
+  localStorage.setItem("wallet", JSON.stringify(wallet));
+  localStorage.setItem("history", JSON.stringify(history));
+  updateUI();
+}
+
+function updateUI() {
+  document.getElementById("btc").innerText = wallet.BTC;
+  document.getElementById("eth").innerText = wallet.ETH;
+  document.getElementById("usdt").innerText = wallet.USDT;
+  document.getElementById("bnb").innerText = wallet.BNB;
+
+  const list = document.getElementById("history");
+  list.innerHTML = "";
+
+  history.forEach(item => {
+    const li = document.createElement("li");
+    li.innerText = item;
+    list.appendChild(li);
+  });
 async function getPrices() {
   const res = await fetch(
     "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether,binancecoin&vs_currencies=eur"
@@ -36,4 +128,5 @@ async function updateUI() {
     li.innerText = item;
     list.appendChild(li);
   });
+}
 }
