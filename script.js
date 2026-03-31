98ba7b2beea74e8282627459395cb590d8f2a618
tu-wallet-html <support@cryptoblockchain.my>
2026-03-31 16:24:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-let balance = 0;
+let balance = localStorage.getItem("balance") || 0;
+balance = Number(balance);
+
+updateUI();
 
 function addMoney() {
-  balance += 10;
+  let amount = Number(document.getElementById("amount").value);
+  if (amount > 0) {
+    balance += amount;
+    save();
+  }
+}
+
+function removeMoney() {
+  let amount = Number(document.getElementById("amount").value);
+  if (amount > 0) {
+    balance -= amount;
+    save();
+  }
+}
+
+function save() {
+  localStorage.setItem("balance", balance);
+  updateUI();
+}
+
+function updateUI() {
   document.getElementById("balance").innerText = "$" + balance;
 }
